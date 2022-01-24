94e758e1e742810481e1bc992777d46ec7d4799f
sigmasoldi3r <pablobc.1995@gmail.com>
2022-01-24 17:05:16 +0100

    Added more tests.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -49,4 +49,29 @@ describe('Iterator-like API', () => {
     }
     expect(i).to.be.eq(TEST_SAMPLE_VALUE)
   })
+  it('Should map the value, if present.', () => {
+    const value = withValue()
+      .map((n) => n + 1)
+      .get()
+    expect(value).to.be.eq(TEST_SAMPLE_VALUE + 1)
+    const noValue = withoutValue().map((n) => n + 1)
+    expect(noValue.isDefined()).to.be.false
+  })
+  it('Should get the value if present, or the alternative.', () => {
+    expect(withValue().getOrElse(0)).to.be.eq(TEST_SAMPLE_VALUE)
+    expect(withoutValue().getOrElse(0)).to.be.eq(0)
+  })
+  it('Should fold the value, if present', () => {
+    const fallback = 'NO VALUE'
+    const a = withValue().fold(fallback, (n) => n.toString())
+    const b = withoutValue().fold(fallback, (n) => n.toString())
+    expect(a).to.be.eq(TEST_SAMPLE_VALUE.toString())
+    expect(b).to.be.eq(fallback)
+  })
+  it('Should flat-map values from optionals', () => {
+    const a = withValue().flatMap((n) => some('hello'))
+    const b = withoutValue().flatMap((n) => some('hello'))
+    expect(a.get()).to.be.eq('hello')
+    expect(b.isDefined()).to.be.false
+  })
 })
